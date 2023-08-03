4bf70e573871e53e0a785ee84fb5013bd9b6dec9
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-08-03 14:19:46 -0300

    Página rehecha con responsive mobile

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,10 @@
-//MENU MOBILE //
+document.addEventListener("DOMContentLoaded", function () {
+ 
+    let boton = document.querySelector(".nav-content-icon");
+    let links = document.querySelector(".links-mobile");
 
-const ham = document.querySelector('.ham');
-const enlaces = document.querySelector('.nav__links');
-
-ham.addEventListener('click', () => {
-    
-    enlaces.classList.toggle('activado');
+    boton.addEventListener("click", ()=> {
+        links.classList.toggle("links-mobile");
+    });
 
 });
-
-$(document).ready(function(){
-
-    //FILTRANDO PRODUCTOS //
-
-    $('.catalogo__list__item').click(function(){
-        var catProductMarca = $(this).attr('category-marca');
-        var catProductColeccion = $(this).attr('category-coleccion');
-
-        // OCULTANDO PRODUCTOS //
-        $('.catalogo__item').hide();
-
-        // MOSTRANDO PRODUCTOS //
-        $('.catalogo__item[category-marca="'+catProductMarca+'"]').show();
-        $('.catalogo__item[category-coleccion="'+catProductColeccion+'"]').show();
-
-    });
-    //MOSTRANDO TODOS LOS PRODUCTOS //
-    $('.catalogo__list__item[category-marca="all"]').click(function(){
-        $('.catalogo__item').show();
-    });
-})
